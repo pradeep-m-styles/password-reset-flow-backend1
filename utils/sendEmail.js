2c0548b9ee86f9971cf5d2063a75de030897023f
Pradeep M <pradeepmuthu1526@gmail.com>
2026-05-14 14:15:56 +0530

    fix: brevo smtp

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,9 @@ const nodemailer = require("nodemailer");
  * Creates a nodemailer transporter using Gmail 
  */
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
