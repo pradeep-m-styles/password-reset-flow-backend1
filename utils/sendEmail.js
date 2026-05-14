9def197a1d679f488f8c4528fd22a25f7330b898
Pradeep M <pradeepmuthu1526@gmail.com>
2026-05-14 13:36:27 +0530

    fix: gmail service for nodemailer on render

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,12 +1,10 @@
 const nodemailer = require("nodemailer");
 
 /**
- * Creates a nodemailer transporter using Gmail SMTP
+ * Creates a nodemailer transporter using Gmail 
  */
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false, // true for 465, false for other ports
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
